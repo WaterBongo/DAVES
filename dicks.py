4e8ccd2370fc27a1a5291f5c14781a0592ffd37e
Rumpkin <orionvillager@gmail.com>
2023-01-28 12:26:32 -0800

    Ipdate

--- dicks.py
+++ dicks.py
@@ -1,48 +1,47 @@
-import discord,threading
+import discord,threading,asyncio
 from discord.ext import commands
 import requests
-def bee():
-    bot = commands.Bot(command_prefix='>', self_bot=True)
 
-    @bot.event
-    async def on_connect():
-        print("connted!")
 
-    @bot.event
-    async def on_message(ctx:commands.Context):
-        if (ctx.guild == None):
-            if (ctx.content == "dick"):
-                
-                await ctx.channel.send('Fuck You Nerd!')
-                
-                block(ctx)
-                closeChannel(ctx)
-                
-        
+bot = commands.Bot(command_prefix='>', self_bot=True)
 
-            print(ctx.content)
+@bot.event
+async def on_connect():
+    print("connted!")
 
-    @bot.command()
-    async def ping(ctx):
-        await ctx.send('pong')
+@bot.event
+async def on_message(ctx:commands.Context):
+    if (ctx.guild == None):
+        if (ctx.content == "dick"):
+            
+            await ctx.channel.send('Fuck You Nerd!')
+            
+            block(ctx)
+            closeChannel(ctx)
+            
+    
 
-    def block(ctx):
-        headers = {"authorization": token, "user-agent": "Mozilla/5.0"}
-        json = {"type": 2}
-        bloke = requests.put(
-            f"https://canary.discord.com/api/v8/users/@me/relationships/{ctx.author.id}",
-            headers=headers,
-            json=json,
-        )
+        print(ctx.content)
 
-    def closeChannel(ctx):
-        headers = {"authorization": token, "user-agent": "Mozilla/5.0"}
-        close = requests.delete(
-            f"https://canary.discord.com/api/v8/channels/{ctx.channel.id}",
-            headers=headers
-        )
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-    token = 'MzM1MTk3MTMyODc5NDI5NjMy.G3D_4N.G8N8d8Oqz36XRhF25iJA9MTBRdXcPUzex84jEY'
-    bot.run(token)
-threading.Thread(target=bee).start()
-print("nice car")
+def block(ctx):
+    headers = {"authorization": token, "user-agent": "Mozilla/5.0"}
+    json = {"type": 2}
+    bloke = requests.put(
+        f"https://canary.discord.com/api/v8/users/@me/relationships/{ctx.author.id}",
+        headers=headers,
+        json=json,
+    )
+
+def closeChannel(ctx):
+    headers = {"authorization": token, "user-agent": "Mozilla/5.0"}
+    close = requests.delete(
+        f"https://canary.discord.com/api/v8/channels/{ctx.channel.id}",
+        headers=headers
+    )
+
+token = 'MzM1MTk3MTMyODc5NDI5NjMy.G3D_4N.G8N8d8Oqz36XRhF25iJA9MTBRdXcPUzex84jEY'
+bot.run(token)
